33e05ca7275bb5e894e539e0e90fc7ba4a1c1b0a
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-14 16:34:54 +0100

    Add ArrayList example and Array

---
+++ src/arrayExample/Array.java
@@ -0,0 +1,65 @@
+package arrayExample;
+
+import java.util.Scanner;
+
+public class Array {
+    // ----------------------- 2
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+
+        // first option
+        int[] myIntArray = new int[10];
+
+        // second option
+        int[] myIntArray2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // third option
+        int[] myIntArray3 = new int[10];
+        for (int i = 0; i < myIntArray3.length; i++) { // better to use length!
+            myIntArray3[i] = i * 10;
+        }
+
+        // printing array version 1
+        for (int i = 0; i < myIntArray3.length; i++) {
+            System.out.println("Element " + i + ", value is " + myIntArray3[i]);
+        }
+        // printing array version 2
+        for (int i = 0; i < myIntArray3.length; i++) {
+            System.out.print(myIntArray3[i]);
+        }
+        System.out.println();
+        System.out.println("---------------");
+
+        // ----------------------- 2
+        int[] myIntegers = getIntegers(5);
+        System.out.println("The average is " + getAverarge(myIntegers));
+        // printing array version with space
+        for (int i = 0; i < myIntegers.length; i++) {
+            System.out.print(myIntegers[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // ----------------------- 2
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer value: \r");
+        int[] values = new int[number];
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+        return values;
+    }
+
+    public static double getAverarge(int[] array) {
+        int sumTotal = 0;
+        for (int i = 0; i < array.length; i++) {
+            sumTotal += array[i];
+        }
+        double average = sumTotal / array.length;
+        return average;
+    }
+
+
+}
---
+++ src/groceryList/Main.java
@@ -0,0 +1,61 @@
+package groceryList;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList();
+
+    public static void main(String[] args) {
+        boolean quit = false;
+        int choice = 0;
+
+        while (!quit) {
+            printMenu();
+            System.out.println("Enter your choice");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+            case 0:
+                printMenu();
+                break;
+            case 1:
+                groceryList.printGroceryList();
+                break;
+            case 2:
+                addItem();
+                break;
+            case 3:
+                // removeItem();
+                break;
+
+            case 8:
+                quit = true;
+                break;
+            }
+        }
+    }
+
+    private static void printMenu() {
+        System.out.print("\n Press ");
+        System.out.print("\n 0 - To print choice options.");
+        System.out.print("\n 1 - To print the list of grocery items.");
+        System.out.print("\n 2 - To add an item to the list.");
+        System.out.print("\n 8 - To quit the application.");
+    }
+
+    private static void addItem() {
+        System.out.println("Please enter grocery item: ");
+        String itemToAdd = scanner.nextLine();
+        groceryList.addGroceryItem(itemToAdd);
+    }
+
+    // private static void removeItem() {
+    //     System.out.println("Enter item number: ");
+    //     int itemNumber = scanner.nextInt();
+    //     scanner.nextLine();
+    //     groceryList.removeGroceryItem(itemNumber - 1);
+    // }
+
+}
